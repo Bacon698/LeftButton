b095e24a39c2b8bd5899664b476a10a994518d7a
张培根 <zhangpeigen@xiaomai.local>
2017-06-09 15:42:22 +0800

    autoloyout

--- LeftButton/ViewController.m
+++ LeftButton/ViewController.m
@@ -21,17 +21,18 @@
     LeftButton *lButton = [[LeftButton alloc]init];
     [lButton setImage:[UIImage imageNamed:@"arrows_right"] forState:UIControlStateNormal];
     [lButton setTitle:@"查看更多" forState:UIControlStateNormal];
-        [lButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [lButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     
     [self.view addSubview:lButton];
-    lButton.frame = CGRectMake(20, 20, 0, 0);
-    [lButton sizeToFit];
-//    NSLayoutConstraint *contraint1 = [NSLayoutConstraint constraintWithItem:lButton attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.0 constant:15];
-//    NSLayoutConstraint *contraint2 = [NSLayoutConstraint constraintWithItem:lButton attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeft multiplier:1.0 constant:15];
+//    lButton.frame = CGRectMake(20, 20, 0, 0);
+//    [lButton sizeToFit];
+    lButton.translatesAutoresizingMaskIntoConstraints = NO;
+    NSLayoutConstraint *contraint1 = [NSLayoutConstraint constraintWithItem:lButton attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.0 constant:30];
+    NSLayoutConstraint *contraint2 = [NSLayoutConstraint constraintWithItem:lButton attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeft multiplier:1.0 constant:15];
 //    NSLayoutConstraint *constrant3 = [NSLayoutConstraint constraintWithItem:lButton attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationGreaterThanOrEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:300.0];
 //    NSLayoutConstraint *constrant4 = [NSLayoutConstraint constraintWithItem:lButton attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationGreaterThanOrEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:20];
-//    NSArray *array = [NSArray arrayWithObjects:contraint1,contraint2,constrant3,constrant4, nil];
-//    [self.view addConstraints:array];
+    NSArray *array = [NSArray arrayWithObjects:contraint1,contraint2, nil];
+    [self.view addConstraints:array];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
